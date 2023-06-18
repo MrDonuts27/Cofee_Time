1dad81d4239fd5768c6d0b26286ef29885d8e0c7
MrDonuts27 <133508622+MrDonuts27@users.noreply.github.com>
2023-06-18 15:37:07 +0700

    Update MapScreen.js

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -12,11 +12,115 @@ const MapScreen = () => {
       title: 'Class cafe ประตู 1',
     },
     {
+      latitude: 14.904933,
+      longitude: 102.010849,
+      title: 'More Milk',
+    },
+    {
+      latitude: 14.881884,
+      longitude: 102.015055,
+      title: 'Faraday Coffee',
+    },
+    {
       latitude: 14.884385,
       longitude: 102.026772,
       title: 'Coffee and cream',
     },
-    // Add more markers as needed
+    {
+      latitude: 14.922146,
+      longitude: 101.993077,
+      title: 'Polar Polar Cafeสาขา1',
+    },
+      {
+      latitude: 14.876306,
+      longitude: 102.043333,
+      title: 'Takeoff Cafe By Otaru @Airplane Park KoratBy Otaru',
+    },
+      {
+      latitude: 14.876379,
+      longitude: 102.033522,
+      title: 'นมหนมFOODมทส.ประตู1',
+    },
+      {
+      latitude: 14.907155,
+      longitude:102.011275,
+      title: 'กาแฟเถื่อนละมุน ',
+    },
+      {
+      latitude: 14.907810,
+      longitude: 102.004021,
+      title: 'Ananya Cafe',
+    },
+      {
+      latitude: 14.902751,
+      longitude: 102.009679,
+      title: 'จุด 3 จุดมทส',
+    },
+      {
+      latitude: 14.885112,
+      longitude: 102.024944,
+      title: 'ลัลลา',
+    },
+        {
+      latitude: 14.909277,
+      longitude: 102.014432,
+      title: 'ลานหอมลานนม ',
+    },
+        {
+      latitude: 14.864268,
+      longitude: 102.036065,
+      title: 'Alert Coffee',
+    },
+        {
+      latitude: 14.885112,
+      longitude: 102.024944,
+      title: 'ลัลลา',
+    },
+        {
+      latitude: 14.878558,
+      longitude: 102.030909,
+      title: 'ปั๊มนม Pอุ้ม',
+    },
+        {
+      latitude: 14.904888,
+      longitude: 102.011198,
+      title: 'HOME cafe',
+    },
+         {
+      latitude: 14.883680,
+      longitude: 102.024923,
+      title: 'Cafe Amazon',
+    },
+         {
+      latitude: 14.899739,
+      longitude: 102.051973,
+      title: 'Sata Cafe',
+    },
+         {
+      latitude: 14.874749,
+      longitude: 102.015339,
+      title: 'POLAR POLAR Campus Cafe',
+    },
+         {
+      latitude: 14.904731,
+      longitude: 102.010880,
+      title: 'The Bloom Cafe',
+    },
+         {
+      latitude: 14.898599,
+      longitude: 102.049968,
+      title: 'Little Hut Cafe',
+    },
+         {
+      latitude: 14.901417,
+      longitude: 102.069500,
+      title: 'Saii Cafe',
+    },
+         {
+      latitude: 14.903162,
+      longitude: 102.056476,
+      title: 'Ob-Oon Bakery&Coffee ',
+    },
   ];
 
   const initialRegion = {
